4e78ab67183b9da1a31bbcbb0088625d08c27730
Miguel Ruiz <myguelruiz@gmail.com>
2015-10-30 13:59:14 +0000

    Start add

---
+++ app.js
@@ -0,0 +1,54 @@
+var app = angular.module('StarterApp', ['ngMaterial']);
+
+app.controller('AppController', function($mdSidenav, $scope) {
+  var vm = this;
+
+  vm.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+  
+  
+   var imagePath = 'img/list/60.jpeg';
+    $scope.phones = [
+      { type: 'Home', number: '(555) 251-1234' },
+      { type: 'Cell', number: '(555) 786-9841' },
+      { type: 'Office', number: '(555) 314-1592' }
+    ];
+    $scope.messages = [
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      }
+   ];
+});
